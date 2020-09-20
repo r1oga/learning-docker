00e992a61e124b48920bcd0871dbb0be930a6b46
r1oga <r1oga@tuta.io>
2020-09-20 10:36:19 +0200

    fix: Add missing space in postgres query

--- api/index.js
+++ api/index.js
@@ -28,7 +28,7 @@ const pgClient = new Pool({
 })
 pgClient.on('connect', () => {
   pgClient
-    .query('CREATE TABLE IF NOT EXISTS values(number INT)')
+    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
     .catch(err => console.log(err))
 })
 
